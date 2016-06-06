824e2c819b4c5dfd773507d7affcbaacbbfe1eee
OferCaspi <fortzi@gmail.com>
2016-06-06 20:32:59 +0300

    FScore calculation

--- src/VerdictWritableArray.java
+++ src/VerdictWritableArray.java
@@ -1,18 +1,20 @@
-import com.sun.corba.se.spi.ior.Writeable;
-import org.omg.CORBA_2_3.portable.OutputStream;
+import org.apache.commons.lang.NotImplementedException;
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
 /**
  *
  * Created by user1 on 06/06/2016.
  */
-public class VerdictWritableArray implements Writeable {
+public class VerdictWritableArray implements Writable {
 
-    double pmi;
     FScorer.Verdict[] verdicts;
 
-    public VerdictWritableArray(int size, double pmi) {
+    public VerdictWritableArray(int size) {
         this.verdicts = new FScorer.Verdict[size];
-        this.pmi = pmi;
     }
 
     public void setVerdict(int i, FScorer.Verdict v) {
@@ -29,21 +31,17 @@ public class VerdictWritableArray implements Writeable {
         return verdicts[i];
     }
 
-    public double getPmi() {
-        return pmi;
-    }
-
-    public void setPmi(double pmi) {
-        this.pmi = pmi;
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        throw new NotImplementedException();
     }
 
     @Override
-    public void write(OutputStream out) {
+    public void write(DataOutput out) throws IOException {
         String[] temp = new String[verdicts.length];
         for (int i = 0; i < temp.length; i++)
             temp[i] = verdicts[i].name();
 
-        out.write_double(pmi);
-        out.write_string(String.join(" ", temp));
+        out.writeUTF(String.join(" ", temp));
     }
 }
